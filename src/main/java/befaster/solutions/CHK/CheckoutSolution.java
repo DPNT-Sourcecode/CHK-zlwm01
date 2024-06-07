4000f7e1dfd15d1d209fdb760054f2847bf2e281
sbx_user1051 <sbx_user1051@169.254.25.57>
2024-06-07 18:55:18 +0000

    Fri Jun 07 18:55:18 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -5,7 +5,12 @@ import befaster.runner.SolutionNotImplementedException;
 public class CheckoutSolution {
     public Integer checkout(String skus) {
         int sum = 0;
+        int ok=0;
+        int cont1 =0;
+        int cont2 =0;
+        char aux;
         for(char c: skus.toCharArray()) {
+            ok++;
             if (c == 'A')
                 sum+=50;
             else if (c == 'B')
@@ -17,7 +22,23 @@ public class CheckoutSolution {
             else
                 return -1;
 
+             aux = c;
+
+            if(ok>1)
+                if(aux == 'A'){
+                    cont1+=1;
+                    cont2 = 0;}
+                else if (aux == 'B'){
+                    cont2+=1;
+                    cont1 = 0;}
+            if(cont1 ==3){
+                sum-=20;
+                cont1 = 0;}
+            if(cont2 ==2){
+                sum-=15;
+                cont2 = 0;}
         }
         return sum;
     }
 }
+
