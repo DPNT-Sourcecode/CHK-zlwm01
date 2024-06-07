185a17fef42dc5aba9676f0da118f71d18b8eb62
sbx_user1051 <sbx_user1051@169.254.25.57>
2024-06-07 18:59:29 +0000

    Fri Jun 07 18:59:29 UTC 2024

--- src/main/java/befaster/solutions/CHK/CheckoutSolution.java
+++ src/main/java/befaster/solutions/CHK/CheckoutSolution.java
@@ -8,9 +8,9 @@ public class CheckoutSolution {
         int ok=0;
         int cont1 =0;
         int cont2 =0;
-        char aux;
+        char aux = ' ';
         for(char c: skus.toCharArray()) {
-            ok++;
+            ok+=1;
             if (c == 'A')
                 sum+=50;
             else if (c == 'B')
@@ -22,13 +22,11 @@ public class CheckoutSolution {
             else
                 return -1;
 
-             aux = c;
-
             if(ok>1)
-                if(aux == 'A'){
+                if(aux == 'A' && aux == c){
                     cont1+=1;
                     cont2 = 0;}
-                else if (aux == 'B'){
+                else if (aux == 'B'&& aux == c){
                     cont2+=1;
                     cont1 = 0;}
             if(cont1 ==3){
@@ -37,8 +35,10 @@ public class CheckoutSolution {
             if(cont2 ==2){
                 sum-=15;
                 cont2 = 0;}
+            aux = c;
         }
         return sum;
     }
 }
+
 
